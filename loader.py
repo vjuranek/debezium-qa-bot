245ff9fbbdf6ec78e44d107a8ee1c91a990c5652
Vojtech Juranek <vojtech.juranek@gmail.com>
2025-01-26 17:58:08 +0100

    Convert loading into the function
    Also add logging.

--- loader.py
+++ loader.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import logging
+
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_milvus import Milvus
 from langchain_ollama import ChatOllama
@@ -19,18 +21,28 @@ URLS = [
     # "https://debezium.io/documentation/reference/3.0/connectors/oracle.html",
 ]
 
+log = logging.getLogger("loader")
+
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
     chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
 embeddings = OllamaEmbeddings(model=EMBEDDINGS_MODEL)
 
-docs = [WebBaseLoader(url).load() for url in URLS]
-doc_splits = text_splitter.split_documents(docs[0])
+def load_from_url(url):
+    log.info(f"Processing URL {url!r}")
+    docs = WebBaseLoader(url).load()
+    doc_split = text_splitter.split_documents([doc for doc in docs])
 
-Milvus.from_documents(
-    documents=doc_splits,
-    embedding=embeddings,
-    connection_args={
-        "host": "127.0.0.1:19530",
-    },
-    drop_old=True,
-)
+    log.info("Loading splited document into the DB")
+    Milvus.from_documents(
+        documents=doc_split,
+        embedding=embeddings,
+        connection_args={
+            "host": "127.0.0.1:19530",
+        },
+        drop_old=True,
+    )
+
+
+if __name__ == "__main__":
+    load_from_url(URLS[0])
+    
