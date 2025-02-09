ce94b4a2003c220400d1f56fb5fbe8c0175b1da7
Vojtech Juranek <vojtech.juranek@gmail.com>
2025-02-09 23:53:31 +0100

    Pass url via request parameter

--- loader.py
+++ loader.py
@@ -3,6 +3,7 @@
 import logging
 
 from flask import Flask
+from flask import request
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_milvus import Milvus
 from langchain_ollama import ChatOllama
@@ -13,14 +14,6 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 CHUNK_SIZE = 250
 CHUNK_OVERLAP = 0
 EMBEDDINGS_MODEL = "llama3.2"
-URLS = [
-    "https://debezium.io/documentation/reference/3.0/tutorial.html",
-    # "https://debezium.io/documentation/reference/3.0/configuration/avro.html",
-    # "https://debezium.io/documentation/reference/3.0/configuration/topic-auto-create-config.html",
-    # "https://debezium.io/documentation/reference/3.0/configuration/signalling.html",
-    # "https://debezium.io/documentation/reference/3.0/configuration/notification.html",
-    # "https://debezium.io/documentation/reference/3.0/connectors/oracle.html",
-]
 
 # configure logging
 logging.basicConfig(
@@ -36,7 +29,7 @@ embeddings = OllamaEmbeddings(model=EMBEDDINGS_MODEL)
 
 @app.route("/load")
 def load_from_url():
-    url = URLS[0]
+    url = request.args.get("url")
     log.info(f"Processing URL {url!r}")
     docs = WebBaseLoader(url).load()
     doc_split = text_splitter.split_documents([doc for doc in docs])
@@ -50,6 +43,8 @@ def load_from_url():
         },
         drop_old=True,
     )
+    log.info(f"Loaded {url} into the DB")
+    
     return f"Loaded {url}"
 
 
